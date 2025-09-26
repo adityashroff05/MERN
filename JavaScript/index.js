efc44a30fe9147638e5f90e2b779906d959b262e
Aditya Kumar <wartinsh1234@gmail.com>
2025-09-26 12:34:45 +0530

    java script

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,4 +1,4 @@
-let name = "Aditya";
+/*let name = "Aditya";
 let age = 25;
 console.log("name:" + name + ",age:" + age);
 
@@ -6,4 +6,13 @@ console.log(name, age);
 
 const account = 24334;
 account = 45;
-console.log(account);
+console.log(account);*/
+
+//number
+let a = 10;
+let b = 56.7;
+let c = "ABC";
+console.log(a, b, c);
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
