a057ff36ac7bf884f3e95b564cff81c1ac76244e
Aditya Kumar <wartinsh1234@gmail.com>
2025-09-29 22:59:28 +0530

    Comparision operator

--- JavaScript/Operators/operators.js
+++ JavaScript/Operators/operators.js
@@ -9,13 +9,23 @@
 // console.log(2**3); //exponentiation operator
 
 //Assignment operator
+// let x = 5;
+// let y = 3;
+// //x = x+y;
+// // x = x-y;
+// // x = x*y;
+// // x = x/y;
+// // x = x%y;
+// // x = x**y;
+// x+=y;
+// console.log(x);
+
+// Comparison operator
 let x = 5;
 let y = 3;
-//x = x+y;
-// x = x-y;
-// x = x*y;
-// x = x/y;
-// x = x%y;
-// x = x**y;
-x+=y;
-console.log(x);
+console.log(x == y); // equal to
+console.log(x != y); // not equal to
+console.log(x > y); // greater than
+console.log(x < y); // less than
+console.log(x >= y); // greater than or equal to
+console.log(x <= y); // less than or equal to
