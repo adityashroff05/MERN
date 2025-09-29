4e98dfb8d6e1cb8e73cf8fcae560b39b93a99f4f
Aditya Kumar <wartinsh1234@gmail.com>
2025-09-29 22:54:52 +0530

    Operator

---
+++ JavaScript/Operators/operators.js
@@ -0,0 +1,9 @@
+//operator
+//Arithemiatic operator
+
+console.log(2+6);
+console.log(6-2);
+console.log(2*6);
+console.log(6/2);
+console.log(7%2); //modulus operator
+console.log(2**3); //exponentiation operator
