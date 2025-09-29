3057973cba4560a5d4cdac1884f49be9f147d7bb
Aditya Kumar <wartinsh1234@gmail.com>
2025-09-29 22:56:42 +0530

    Assignment operator

--- JavaScript/Operators/operators.js
+++ JavaScript/Operators/operators.js
@@ -1,9 +1,20 @@
 //operator
 //Arithemiatic operator
 
-console.log(2+6);
-console.log(6-2);
-console.log(2*6);
-console.log(6/2);
-console.log(7%2); //modulus operator
-console.log(2**3); //exponentiation operator
+// console.log(2+6);
+// console.log(6-2);
+// console.log(2*6);
+// console.log(6/2);
+// console.log(7%2); //modulus operator
+// console.log(2**3); //exponentiation operator
+
+//Assignment operator
+let x = 5;
+let y = 3;
+//x = x+y;
+// x = x-y;
+// x = x*y;
+// x = x/y;
+// x = x%y;
+ x = x**y;
+console.log(x);
