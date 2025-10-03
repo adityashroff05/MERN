a8ad11bba4840c954472cb2f13c01378e7178e2c
Aditya Kumar <wartinsh1234@gmail.com>
2025-10-03 09:43:04 +0530

    Number

--- JavaScript/Numbers/basic.js
+++ JavaScript/Numbers/basic.js
@@ -1,4 +1,12 @@
-let a = 65;
-let b = 657.988;
-console.log(b.toFixed(2))
-console.log(a+b);
+// let a = 65;
+// let b = 657.988;
+// console.log(b.toFixed(2)); // 2 decimal places
+// console.log(b.toPrecision(4)); // 4 significant digits
+// console.log(b.toString()); // string representation
+//------------------------------------------------
+
+let a = new Number(123); // Number object
+let b = 456; // primitive number
+console.log(a);
+console.log(b);
+console.log(a*b);
