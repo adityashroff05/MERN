dbae376fd114713dd576251393ba97f5952ed63c
Aditya Kumar <wartinsh1234@gmail.com>
2025-10-09 22:31:49 +0530

    Scope

---
+++ JavaScript/Scope and Higher order function/Scope.js
@@ -0,0 +1,20 @@
+// Scope and Clouser, and higher order functions
+//Global Scope
+var a = "Hello World";
+//Function Scope
+function greet() {
+  var b = "Hello Everyone";
+  console.log(a);
+  console.log(b);
+}
+//Block Level Scope
+if (true) {
+  let c = "Hello Aditya";
+  const d = "Hello Unknown";
+  console.log(c);
+  console.log(d);
+}
+greet();
+// console.log(b); // Error: b is not defined
+// console.log(c); // Error: c is not defined
+// console.log(d); // Error: d is not defined
