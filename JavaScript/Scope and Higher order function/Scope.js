a82f07f8efc43c1d5a938eaefc8fa46b7a825916
Aditya Kumar <wartinsh1234@gmail.com>
2025-10-10 23:42:32 +0530

    Scope

--- JavaScript/Scope and Higher order function/Scope.js
+++ JavaScript/Scope and Higher order function/Scope.js
@@ -1,12 +1,16 @@
 // Scope and Clouser, and higher order functions
 //Global Scope
 var a = "Hello World";
+
+
 //Function Scope
 function greet() {
   var b = "Hello Everyone";
   console.log(a);
   console.log(b);
 }
+
+
 //Block Level Scope
 if (true) {
   let c = "Hello Aditya";
@@ -15,6 +19,9 @@ if (true) {
   console.log(d);
 }
 greet();
+
 // console.log(b); // Error: b is not defined
 // console.log(c); // Error: c is not defined
 // console.log(d); // Error: d is not defined
+//Var respect only function scope
+//Let and Const respect all level of scope
