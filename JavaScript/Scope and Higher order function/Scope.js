725fd8d1f26b4efe138087029b30db1b5abfe707
Aditya Kumar <wartinsh1234@gmail.com>
2025-10-13 22:44:59 +0530

    clouser

--- JavaScript/Scope and Higher order function/Scope.js
+++ JavaScript/Scope and Higher order function/Scope.js
@@ -26,17 +26,38 @@
 //Var respect only function scope
 //Let and Const respect all level of scope
 
-function createCounter() {
-  let count = 0;
-  function increament(){
-    //console.log("I am Increament function");
-    count++;
-    return count;
+// function createCounter() {
+//   let count = 0;
+//   function increament(){
+//     //console.log("I am Increament function");
+//     count++;
+//     return count;
+//   }
+//   return increament;
+// }
+// const counter = createCounter();
+// console.log(counter);
+// console.log(counter());
+// console.log(counter());
+// console.log(counter());
+
+//-------------------------------------------------------
+//Clouser Example
+let user = {
+  balance: 200,
+  withdraw: function(amount){
+    if(typeof amount==="number" && amount>0 && amount<=this.balance){
+      this.balance-=amount;
+      return this.balance;
+    }
+  },
+  deposit: function(amount){
+    if(typeof amount==="number" && amount>0){
+      this.balance+=amount;
+      return this.balance;
+    }
   }
-  return increament;
 }
-const counter = createCounter();
-console.log(counter);
-console.log(counter());
-console.log(counter());
-console.log(counter());
+console.log(user.deposit(100));
+console.log(user.withdraw(50));
+console.log(user.withdraw(3));
