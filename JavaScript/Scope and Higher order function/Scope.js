ba6061e86ef119d60c7a1cc3c7645488cb734c51
Aditya Kumar <wartinsh1234@gmail.com>
2025-10-13 22:30:31 +0530

    closure

--- JavaScript/Scope and Higher order function/Scope.js
+++ JavaScript/Scope and Higher order function/Scope.js
@@ -1,27 +1,42 @@
 // Scope and Clouser, and higher order functions
 //Global Scope
-var a = "Hello World";
+// var a = "Hello World";
 
 
 //Function Scope
-function greet() {
-  var b = "Hello Everyone";
-  console.log(a);
-  console.log(b);
-}
+// function greet() {
+//   var b = "Hello Everyone";
+//   console.log(a);
+//   console.log(b);
+// }
 
 
 //Block Level Scope
-if (true) {
-  let c = "Hello Aditya";
-  const d = "Hello Unknown";
-  console.log(c);
-  console.log(d);
-}
-greet();
+// if (true) {
+//   let c = "Hello Aditya";
+//   const d = "Hello Unknown";
+//   console.log(c);
+//   console.log(d);
+// }
+// greet();
 
 // console.log(b); // Error: b is not defined
 // console.log(c); // Error: c is not defined
 // console.log(d); // Error: d is not defined
 //Var respect only function scope
 //Let and Const respect all level of scope
+
+function createCounter() {
+  let count = 0;
+  function increament(){
+    //console.log("I am Increament function");
+    count++;
+    return count;
+  }
+  return increament;
+}
+const counter = createCounter();
+console.log(counter);
+console.log(counter());
+console.log(counter());
+console.log(counter());
